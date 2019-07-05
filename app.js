3038bcf1f4db54c0ddf0e3bdb622dbe9645691f6
Wilfredo Murillo <wilmugo@gmail.com>
2019-07-05 19:53:36 -0400

    test find method mongoose

--- app.js
+++ app.js
@@ -29,8 +29,49 @@ const fruit = new Fruit ({
 
 const person = new Person ({
   name: "Wilfredo",
-  
+  age: 36
 });
 
 //fruit.save();
 //person.save();
+
+const orange = new Fruit ({
+  name: "Orange",
+  rating: 7,
+  review: "Amazing"
+});
+const banana = new Fruit ({
+  name: "Banana",
+  rating: 8,
+  review: "is  a yellow fruit"
+});
+const kiwi = new Fruit ({
+  name: "Kiwi",
+  rating: 5,
+  review: "i dont know kiwi"
+});
+
+// insert many item in a db
+//Fruit.insertMany([kiwi, orange, banana], (err) => {
+
+//  if(err){
+//    console.log(err);
+//  } else {
+//    console.log("Succesfuly saved all the fruits to fruitsDB");
+//  }
+
+//});
+
+// find in a db
+
+Fruit.find((err, fruits) => {
+  if (err) {
+    console.log(err);
+  } else {
+    mongoose.connection.close();
+    //console.log(fruits);
+    fruits.forEach( item => console.log(item.name) );
+  }
+});
+
+
